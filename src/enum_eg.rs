36fe7501501792a9e66ade316e5ec5c72515293c
Nitin Londhe <nitin.londhe@genmills.com>
2022-07-11 02:21:48 +0530

    Message enum whose variants each store different amounts and types of values

--- src/enum_eg.rs
+++ src/enum_eg.rs
@@ -14,3 +14,23 @@ pub enum IpAddr2 {
     V4(String),
     V6(String),
 }
+
+#[derive(Debug)]
+pub enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+pub enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    pub fn call(&self) {
+        // method body would be defined here
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub mod enum_eg;
 
-use enum_eg::{IpAddrKind1, IpAddr1, IpAddr2};
+use enum_eg::{IpAddrKind1, IpAddr1, IpAddr2, IpAddr3, Message};
 
 fn main() {
     // combination of struct & enum
@@ -18,10 +18,17 @@ fn main() {
 
     // without struct & using only enum
     let home2 = IpAddr2::V4(String::from("127.0.0.1"));
-
     let loopback2 = IpAddr2::V6(String::from("::1"));
-
     println!("{:?} {:?}", home2, loopback2);
+
+    // storing different data type
+    let home3 = IpAddr3::V4(127, 0, 0, 1);
+    let loopback3 = IpAddr3::V6(String::from("::1"));
+    println!("{:?} {:?}", home3, loopback3);
+
+    //  Message enum whose variants each store different amounts and types of values
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
 
 
