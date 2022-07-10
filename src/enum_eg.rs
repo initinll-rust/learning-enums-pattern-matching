2da8b69bdf45c04cd95cc91f8931565b69646bf5
Nitin Londhe <nitin.londhe@genmills.com>
2022-07-11 02:17:11 +0530

    storing data in enum

--- src/enum_eg.rs
+++ src/enum_eg.rs
@@ -1,11 +1,16 @@
 #[derive(Debug)]
-pub enum IpAddrKind {
+pub enum IpAddrKind1 {
     V4,
     V6,
 }
+#[derive(Debug)]
+pub struct IpAddr1 {
+    pub kind: IpAddrKind1,
+    pub address: String,
+}
 
 #[derive(Debug)]
-pub struct IpAddr {
-    pub kind: IpAddrKind,
-    pub address: String,
+pub enum IpAddr2 {
+    V4(String),
+    V6(String),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 pub mod enum_eg;
 
-use enum_eg::{IpAddrKind, IpAddr};
-
+use enum_eg::{IpAddrKind1, IpAddr1, IpAddr2};
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
+    // combination of struct & enum
+    let home1 = IpAddr1 {
+        kind: IpAddrKind1::V4,
         address: String::from("127.0.0.1"),
     };
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
+    let loopback1 = IpAddr1 {
+        kind: IpAddrKind1::V6,
         address: String::from("::1"),
     };
-    println!("{:?} {:?}", home, loopback);
+
+    println!("{:?} {:?}", home1, loopback1);
+
+    // without struct & using only enum
+    let home2 = IpAddr2::V4(String::from("127.0.0.1"));
+
+    let loopback2 = IpAddr2::V6(String::from("::1"));
+
+    println!("{:?} {:?}", home2, loopback2);
 }
 
 
